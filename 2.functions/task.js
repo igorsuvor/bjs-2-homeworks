792a43571504ddec626b24056b3e75bcd6967814
Igor Suvorov <2195183@mail.ru>
2021-09-02 22:41:03 +0500

    Completed homework 2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,12 +3,9 @@ function getArrayParams(arr) {
   let min = 100, max = -100, sum = 0, avg;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-    if (arr[i] > max) {
-      max = arr[i];
-    }
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+    
   sum += arr[i];
   avg = Number((sum / arr.length).toFixed(2));
   }
@@ -27,16 +24,15 @@ function worker(arr) {
 }
 
 function makeWork(arrOfArr, func) {
-  let max = -100;
+  let max = -Infinity;
   
   for (let i = 0; i < arrOfArr.length; i++) {
     let sumArr = func(arrOfArr[i]);
 
-    if (sumArr > max) {
-      max = sumArr;
-    }
+    if (sumArr > max) max = sumArr;
   }
-    return max
+  
+  return max
 }
 
 // Задание 3
@@ -44,12 +40,8 @@ function worker2(arr) {
   let min = 100, max = -100;
 
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < min) {
-        min = arr[i];
-      }
-      if (arr[i] > max) {
-        max = arr[i];
-      }
+      if (arr[i] < min) min = arr[i]; 
+      if (arr[i] > max) max = arr[i];
     }
 
   return max - min;
