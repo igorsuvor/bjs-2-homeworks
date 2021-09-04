13b09efb7d3167523c988fd5e8edf9b53525129e
Igor Suvorov <2195183@mail.ru>
2021-09-04 18:01:16 +0500

    Completed homework 3.arrays_2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,13 @@
 // Задача № 1 
 function compareArrays(arr1, arr2) {
-  let result = arr1.every((item, idx) => {
-    return item === arr2[idx] && arr1.length === arr2.length
-  });
-  return result;
+  //let result = arr1.every((item, idx) => {
+  return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]) 
+  // });
+  // return result;
 }
 
 // Задача № 2
 function advancedFilter(arr) {
-  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
-
-  return resultArr;
+  return arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
+ // return resultArr;
 }
